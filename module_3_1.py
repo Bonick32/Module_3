3d3c37d64f230f77c18a5eb7b4f1a68501798cbd
Bonick32 <Bonick3244@gmail.com>
2024-11-29 22:07:50 +0300

    3_1

---
+++ module_3_1.py
@@ -0,0 +1,26 @@
+calls = 0
+def count_calls():
+    global calls
+    calls = calls + 1
+
+def string_info(string):
+    count_calls()
+    tuple_ = (len(string), string.upper(), string.lower())
+    print(tuple_)
+
+def is_contains(string, list_to_search):
+    count_calls()
+    list_to_search = [s.casefold() for s in list_to_search]
+    if string.casefold() in list_to_search:
+        print(True)
+    else:
+        print(False)
+
+string_info('Флегматик')
+string_info('СаНгвинИк')
+string_info('Synchrophasotron')
+
+is_contains('Урбан', ['бан', 'БаНаН', 'урБАН'])
+is_contains('цикл', ['цикличный', 'циклоп'])
+
+print(calls)
