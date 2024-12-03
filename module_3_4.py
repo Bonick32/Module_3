7ff57d3c8cbe72f1b8b9c43f0d67c544e8661953
Bonick32 <Bonick3244@gmail.com>
2024-12-04 00:46:04 +0300

    3_4

--- module_3_4.py
+++ module_3_4.py
@@ -11,8 +11,8 @@ def single_root_words(root_word, *other_words):
         if other_words_low[i] in root_word:
             same_words.append(other_words[i])
 
-    # unique_same_words = []                    # если вдруг в списке попадется одинаковое слово - удалит дубликат из-за
-    # for word in same_words:                   # срабатывания двух циклов for.
+    # unique_same_words = []                    # если вдруг в списке попадется одинаковое c заданным слово -
+    # for word in same_words:                   # удалит дубликат из-за срабатывания двух циклов for.
     #     if word not in unique_same_words:     # Единственный недостаток - если слово повторится во втором случае, то
     #         unique_same_words.append(word)    # оно останется на первом месте в списке, независимо, где оно было в списке
 
