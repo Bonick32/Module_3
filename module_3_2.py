37a757465493cb9f2c304439ece03ce16e003a4f
Bonick32 <Bonick3244@gmail.com>
2024-12-04 00:43:51 +0300

    3_4

--- module_3_2.py
+++ module_3_2.py
@@ -1,14 +1,31 @@
-def send_email(message, recipient, sender = "university.help@gmail.com"):
-    relevant = ['@', '.com', '.ru', '.net']
-    dog1 = recipient[recipient.rfind('@'):recipient.rfind('@') + 1]
-    dog2 = sender[sender.rfind('@'):sender.rfind('@') + 1]
-    dut1 = recipient[recipient.rfind('.'):]
-    dut2 = sender[sender.rfind('.'):]
+# def send_email(message, recipient, sender = "university.help@gmail.com"):
+#     relevant = ['@', '.com', '.ru', '.net']
+#     dog1 = recipient[recipient.rfind('@'):recipient.rfind('@') + 1]
+#     dog2 = sender[sender.rfind('@'):sender.rfind('@') + 1]
+#     dut1 = recipient[recipient.rfind('.'):]
+#     dut2 = sender[sender.rfind('.'):]
+#
+#     if dut1 and dut2 not in relevant:
+#         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+#     elif dog2 and dog1 not in relevant:
+#         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} - некорректно указан email' )
+#     else:
+#         if recipient == sender:
+#             print('Невозможно отправить письмо самому себе!')
+#         else:
+#             if sender == "university.help@gmail.com":
+#                 print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+#             else:
+#                 print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
 
-    if dut1 and dut2 not in relevant:
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-    elif dog2 and dog1 not in relevant:
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} - некорректно указан email' )
+def gram_fail(email):
+    if '@' not in email or not email.endswith((".com",".ru",".net")):
+        return True
+    return False
+
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    if any(map(gram_fail,(sender, recipient))):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
     else:
         if recipient == sender:
             print('Невозможно отправить письмо самому себе!')
---
+++ module_3_4.py
@@ -0,0 +1,23 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_word = root_word.lower()
+    other_words_low = [word.lower() for word in other_words]
+
+    for i in range(len(other_words_low)):
+        if root_word in other_words_low[i]:
+            same_words.append(other_words[i])
+
+    for i in range(len(other_words_low)):
+        if other_words_low[i] in root_word:
+            same_words.append(other_words[i])
+
+    # unique_same_words = []                    # если вдруг в списке попадется одинаковое слово - удалит дубликат из-за
+    # for word in same_words:                   # срабатывания двух циклов for.
+    #     if word not in unique_same_words:     # Единственный недостаток - если слово повторится во втором случае, то
+    #         unique_same_words.append(word)    # оно останется на первом месте в списке, независимо, где оно было в списке
+
+    return print(same_words)
+
+
+single_root_words('Сад', 'Садовник', 'Сажать', 'посадка', 'рассада', 'Садовод',)
+single_root_words('Международный', 'Народ', 'Междуречье', 'Род', 'Народная', 'рОД' ,'Родной',)
